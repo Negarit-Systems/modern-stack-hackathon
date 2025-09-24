ff62fa2a8aea49ee9799dad41a72ae487b072208
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2025-09-24 12:15:22 +0300

    edits

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,1 +1,12 @@
-rafce
+import AuthForm from '@/components/forms/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  )
+}
+
+export default page
