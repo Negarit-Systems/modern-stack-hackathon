b4b81e0c45f0886de4424442401fe148c2c2c576
Yonatan Tizazu <yonatantizazu12@gmail.com>
2025-09-29 11:12:28 +0300

    feat: implement group chat crud

--- convex/crud/groupChat.ts
+++ convex/crud/groupChat.ts
@@ -2,45 +2,44 @@ import { v } from "convex/values";
 import { mutation, query } from "../_generated/server";
 import { makePartial } from "../utils/utils";
 import { groupChatSchema } from "../schemas";
+import { authComponent } from "../auth";
+import { paginationOptsValidator } from "convex/server";
 
-// QUERIES
-export const get = query({
-  handler: async (ctx) => {
-    const items = await ctx.db.query("groupChats").collect();
-    return items;
-  },
-});
-
-export const getOne = query({
-  args: { id: v.id("groupChats") },
-  handler: async (ctx, { id }) => {
-    const item = await ctx.db.get(id);
-    return item;
-  },
-});
-
-// MUTATIONS
-export const create = mutation({
-  args: { item: v.object(groupChatSchema) },
-  handler: async (ctx, { item }) => {
-    const id = await ctx.db.insert("groupChats", item);
-    return id;
-  },
-});
-
-export const update = mutation({
+export const sendMessage = mutation({
   args: {
-    id: v.id("groupChats"),
-    updates: v.object(makePartial(groupChatSchema)),
+    sessionId: v.id("sessions"),
+    content: v.string(),
   },
-  handler: async (ctx, { id, updates }) => {
-    await ctx.db.patch(id, updates);
+  handler: async (ctx, args) => {
+    const authUser = await authComponent.getAuthUser(ctx);
+    if (!authUser) {
+      throw new Error("Not authenticated");
+    }
+    const session = await ctx.db.get(args.sessionId);
+
+    if (!session || !session.collaboratorIds.includes(authUser._id)) {
+      throw new Error("User not in session");
+    }
+
+    return await ctx.db.insert("groupChats", {
+      sessionId: args.sessionId,
+      senderId: authUser._id,
+      content: args.content,
+      senderName: authUser.name,
+    });
   },
 });
 
-export const deleteOne = mutation({
-  args: { id: v.id("groupChats") },
-  handler: async (ctx, { id }) => {
-    await ctx.db.delete(id);
+export const getMessages = query({
+  args: {
+    sessionId: v.id("sessions"),
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("groupChats")
+      .withIndex("by_sessionId", (q) => q.eq("sessionId", args.sessionId))
+      .order("desc")
+      .paginate(args.paginationOpts);
   },
 });
